093f04349fdf84c31674d2ba484e67b45751b4f9
shelly <c7775275@gmail.com>
2025-08-04 18:08:56 +0800

    更新：page名稱

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-react-tool/',
+  base: '/vol-people-tool/',
   plugins: [react()],
 })
 
